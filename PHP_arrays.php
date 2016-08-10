70978811994e1fa09a43015455515079551b38fe
wiredecho <wiredecho@gmail.com>
2016-08-10 03:34:57 -0700

    arrays and conditionals

--- PHP_arrays.php
+++ PHP_arrays.php
@@ -16,16 +16,19 @@
 
 	// ASSOCIATIVE ARRAY
 	$class = array("course" => "HTML Fun", "course_number" => 101);
-	echo $class["course"] . "<br>";
+	echo $class["course"] . ' ' . $class["course_number"] . "<br>";
 	echo $class["course_number"] . "<br>";
 
 
 	// REFERENCING THROUGH THE ARRAY
-	echo "This is student 0:" . $students[0] . ' ' . $instructors[0][1] . '<br>';
-	echo "This is student 1:" . $students[1] . ' taught by ' . $instructors[2][0] . '<br>';
-	echo "This is student 2:" . $students[2] . ' is enjoying ' . $instructors[1][1] . '<br>';
-	echo "This is student 3:" . $students[3] . ' is now teaching ' . $class["course"] .'<br>';
+	echo "This is student 0: " . $students[0] . ' ' . $instructors[0][1] . '<br>';
+	echo "This is student 1: " . $students[1] . ' taught by ' . $instructors[2][0] . '<br>';
+	echo "This is student 2: " . $students[2] . ' is enjoying ' . $instructors[1][1] . '<br>';
+	echo "This is student 3: " . $students[3] . ' is now teaching ' . $class["course"] .'<br>' . '<br>';
 
 
+	var_dump($students);
+	var_dump($instructors);
+	var_dump($class);
 
  ?>
---
+++ PHP_conditionals_operators.php
@@ -0,0 +1,31 @@
+<?php 
+
+	// if - else
+	$subject = "English";
+	if($subject == "Math"){
+		echo "The subject is Math";
+	}else{
+		echo "The subject is English";
+	}
+	
+
+	// switch case
+
+	$first_name = "Phil";
+	$roles = array('programmer', 'Lawyer', 'Doctor');
+	$role = 0;
+	switch($first_name)
+	{
+		case 'Tom':
+			$role = 1;
+		break;
+
+		case 'Phil':
+			$role = 0;
+		break;
+	}
+	echo "Hello $first_name ! Your job is to be $roles[$role]";
+
+
+
+ ?>
