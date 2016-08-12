ae4fa0ddb567985b207448f2fbb8fb43e2d362ad
wiredecho <wiredecho@gmail.com>
2016-08-11 23:09:37 -0700

    finishing touches

--- PHP_php_with_mysql_Quotes/index.php
+++ PHP_php_with_mysql_Quotes/index.php
@@ -11,29 +11,31 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
           
 
-NPM
+
 </head>
 <body>
-	<h1>Welcome to Wise Quotes and some not so much</h1>
-<?php
-	if(isset($_SESSION['message']))
-		echo "<p>". $_SESSION['message'] . "</p>";
-	
-	unset($_SESSION['message']);		
-?>
-	<form action="process.php" method="post">
-		<label>Your Name: 
-			<input type="text" name="name" />
-		</label>
-		<br />
-		<label>Your Quote
-			<textarea name="quote" ></textarea>
-		</label>
-		<br />
-		<input type="submit" value="Add My Quote" />	
-	</form>
-	<form action="main.php" method="post">
-		<input type="submit" value="Skip to Quotes!" />
-	</form>
+	<div class="container">
+		<h4>Welcome to Wise Quotes and some not so much</h4>
+	<?php
+		if(isset($_SESSION['message']))
+			echo "<p>". $_SESSION['message'] . "</p>";
+		
+		unset($_SESSION['message']);		
+	?>
+		<form action="process.php" method="post">
+			<label>Author: 
+				<input type="text" name="name" />
+			</label>
+			<br />
+			<label>Your Quote
+				<textarea name="quote" ></textarea>
+			</label>
+			<br />
+			<input type="submit" value="Add My Quote" />	
+		</form>
+		<form action="main.php" method="post">
+			<input type="submit" value="Skip to Quotes!" />
+		</form>
+	</div>
 </body>
 </html>
--- PHP_php_with_mysql_Quotes/main.php
+++ PHP_php_with_mysql_Quotes/main.php
@@ -7,30 +7,39 @@
 
 <!DOCTYPE HTML>
 <html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Quotes using PHP with MySQL</title>
-	<!-- Compiled and minified CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<head>
+		<meta charset="UTF-8">
+		<title>Quotes using PHP with MySQL</title>
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
-	<!-- Compiled and minified JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
-</head>
-<body>
-	<h1>Quotes</h1>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+	</head>
+	<style type="text/css">
+		.qme{
+			color:orange;
+		}
+		#title{
+			color:silver;
+		}
 
-<?php
-	//var_dump($quotes);
-	foreach($quotes as $quote)
-	{
-		$date_added = date('g:ia F d, Y', strtotime($quote['created_at']));
-		
-		echo "<p>'". $quote['text'] . "'</p>
-				<p> - ". $quote['name'] ." at  ". $date_added ."</p><hr />";
+	</style>
+	<body>
+		<h1 id="title">Quotes</h1>
+
+	<?php
+		//var_dump($quotes);
+		foreach($quotes as $quote)
+		{
+			$date_added = date('g:ia F d, Y', strtotime($quote['created_at']));
 			
-	}
-?>
-	<p><a href='index.php'>Go Back</a></p>
+			echo "<h5 class='qme'>'". $quote['text'] . "'</h5>
+					<p> - ". $quote['name'] ." at  ". $date_added ."</p><hr />";
+				
+		}
+	?>
+		<center><p><a href='index.php' class="btn-floating btn-large waves-effect waves-light red">+</a></p><center>
 
-</body>
+	</body>
 </html>
